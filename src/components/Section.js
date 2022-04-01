0fa827168c184651bdf2cfb0577c78b7dd3f4435
Al <Ros>
2022-04-01 11:18:30 +0300

    Pass props for different models

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
   return (
-    <Wrap>
+    <Wrap bgImage={ backgroundImg }>
         <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online for Touchless Delivery</p>
+          <h1>{ title }</h1>
+          <p>{ description }</p>
         </ItemText>
         <Buttons>
           <ButtonGroup>
             <LeftButton>
-              Custom Order
+            { leftBtnText }
             </LeftButton>
             <RightButton>
-              Existing Inventory 
+            { rightBtnText }
             </RightButton>
           </ButtonGroup>
           <DownArrow src="/images/down-arrow.svg"/>
@@ -31,11 +31,12 @@ const Wrap = styled.div `
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("/images/model-s.jpg");
   display: flex;
   flex-direction: column;
   justify-content: space-between;     // vertical
-  align-items: center;      //horizontal 
+  align-items: center;      //horizontal
+  background-image: ${props => `url("/images/${props.bgImage}")`}; 
+
 `
 
 const ItemText = styled.div`
