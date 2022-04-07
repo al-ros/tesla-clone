719aeb0c0c2d41431f3ffa4757d29a3b6b6364c8
Al Ros <alros.webdev@gmail.com>
2022-04-07 12:11:14 +0300

    Burger menu - finish - add stater for open and close Create carSlice.js in features/car folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false)
+
   return (
     <Container>
       <a>
@@ -18,11 +20,11 @@ function Header() {
       <RightMenu>
         <a href='#'>Shop</a>
         <a href='#'>Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={ () => setBurgerStatus(true)}/>
       </RightMenu>
-      <BurgerNav>
+      <BurgerNav show={burgerStatus} >
         <CloseWrapper>
-          <CustomClose />
+          <CustomClose onClick={ () => setBurgerStatus(false)}/>
         </CloseWrapper>
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
@@ -97,6 +99,8 @@ const BurgerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.2s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
@@ -108,7 +112,7 @@ const BurgerNav = styled.div`
 
 `
 const CustomClose = styled(CloseIcon)`
-
+  cursor: pointer;
 `
 const CloseWrapper = styled.div`
   display: flex;
